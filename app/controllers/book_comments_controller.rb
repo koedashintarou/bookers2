9e637af5f04d25d635cf5de2915e121bfb4f677e
koedashintarou <s20155@s.kaetsu.ac.jp>
2021-03-16 08:26:31 +0000

    コメント

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -11,6 +11,7 @@ class BookCommentsController < ApplicationController
       BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
       redirect_to book_path(params[:book_id])
     end
+    
    private
 
   def book_comment_params
